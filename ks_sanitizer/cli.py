7a45be2da07ba3d47b5663a58031c71421e18a8a
Marcin Kasznia <kasmar00@gmail.com>
2025-12-07 00:00:37 +0100

    Combine feeds for KŚ

--- ks_sanitizer/cli.py
+++ ks_sanitizer/cli.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+import zipfile
 import impuls
 from impuls.model import Date
 from impuls.multi_file import (
@@ -11,7 +13,7 @@ from impuls.resource import HTTPResource, ZippedResource
 from kw_sanitizer.consts import GTFS_HEADERS
 
 
-class LKACombiner(impuls.App):
+class KolejeSlaskieSanitizer(impuls.App):
     def prepare(
         self,
         args: argparse.Namespace,
@@ -21,17 +23,37 @@ class LKACombiner(impuls.App):
             options=options,
             intermediate_provider=SourceFeedProvider(),
             intermediate_pipeline_tasks_factory=lambda feed: [
+                FixInitially(feed),
                 impuls.tasks.LoadGTFS(feed.resource_name)
             ],
             final_pipeline_tasks_factory=lambda _: [
                 impuls.tasks.SaveGTFS(
                     headers=GTFS_HEADERS,
-                    target="out/lka_combined.zip",
+                    target="out/ks.zip",
                 )
             ],
             additional_resources={},
         )
 
+class FixInitially(impuls.Task):
+    def __init__(self, res: IntermediateFeed) -> None:
+        self.res = res 
+        super().__init__()
+
+    def execute(self, r: impuls.TaskRuntime) -> None:
+        file = r.resources[self.res.resource_name]
+        with zipfile.ZipFile(f"fixed-{self.res.version}.out.zip", "w") as zout:
+            with zipfile.ZipFile(file.open_binary()) as zin:
+                for item in zin.infolist():
+                    if item.filename != "shapes.txt":
+                        zout.writestr(item, zin.read(item.filename))
+                    else:
+                        content = zin.read(item.filename).decode("utf-8")
+                        processed_lines = [
+                            line.replace(" ", "") for line in content.splitlines()
+                        ]
+                        zout.writestr("shapes.txt", "\n".join(processed_lines))
+        os.replace(f"fixed-{self.res.version}.out.zip", file.stored_at)
 
 class SourceFeedProvider(IntermediateFeedProvider[ZippedResource]):
     def __init__(self, for_date: Date | None = None) -> None:
@@ -39,16 +61,16 @@ class SourceFeedProvider(IntermediateFeedProvider[ZippedResource]):
 
     def needed(self) -> list[IntermediateFeed[ZippedResource]]:
         URLS = {
-            "2024-12-15": "https://kolej-lka.pl/pliki/pn0e6eg45qcl4hd5/gtfs-2024-2025/zip/",
-            "2025-12-14": "https://kolej-lka.pl/pliki/pn0e6eg45qcl4hd5/gtfs-2025-2026/zip/",
+            "2024-12-15": "https://koleje-ks.pl/gtfs/2024-2025.zip",
+            "2025-12-14": "https://koleje-ks.pl/gtfs/2025-2026.zip",
         }
         feeds: list[IntermediateFeed[ZippedResource]] = []
 
         for key, url in URLS.items():
             feed = IntermediateFeed(
                 HTTPResource.get(url),
-                resource_name=f"lka-{key}.zip",
-                version=f"lka-{key}",
+                resource_name=f"ks-{key}.zip",
+                version=f"ks-{key}",
                 start_date=Date.from_ymd_str(key),
             )
             feeds.append(feed)
@@ -58,4 +80,4 @@ class SourceFeedProvider(IntermediateFeedProvider[ZippedResource]):
 
 
 def main() -> None:
-    LKACombiner().run()
+    KolejeSlaskieSanitizer().run()
